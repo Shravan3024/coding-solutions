e0371e9206e6234a54fa6e17fa7b4f20c5b0e741
Shravan Navale <shravan.n.024@gmail.com>
2026-08-10 14:04:23 +0530

    ✅ Stone Game IV | Hard | Java | Runtime: 2 ms (100.00%) | Memory: 42.2 MB | 2026-08-10

--- leetcode/hard/1510-stone-game-iv/solution.java
+++ leetcode/hard/1510-stone-game-iv/solution.java
@@ -1,21 +1,17 @@
 class Solution {
     public boolean winnerSquareGame(int n) {
-
         boolean[] dp = new boolean[n + 1];
 
-        dp[0] = false;
-
-        for (int i = 1; i <= n; i++) {
-
-            for (int j = 1; j * j <= i; j++) {
-
-                if (!dp[i - j * j]) {
-                    dp[i] = true;
-                    break;
+        for(int i = 0; i <= n; i++) {
+            if(!dp[i]) {
+                for(int j = 1; i + j * j <= n; j++) {
+                    dp[i + j * j] = true;
+                }
+                if(dp[n]) {
+                    return true;
                 }
             }
         }
-
-        return dp[n];
+        return false;
     }
 }
