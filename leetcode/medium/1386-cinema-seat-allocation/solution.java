fdfaca18d024a666a23bbb9450960fbe375bf3c6
Shravan Navale <shravan.n.024@gmail.com>
2026-08-19 23:36:28 +0530

    ✅ Cinema Seat Allocation | Medium | Java | Runtime: 22 ms (53.42%) | Memory: 49.1 MB | 2026-08-19

---
+++ leetcode/medium/1386-cinema-seat-allocation/solution.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+class Solution {
+    public int maxNumberOfFamilies(int n, int[][] reservedSeats) {
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        // Store reserved seats for each row using a bitmask
+        for (int[] seat : reservedSeats) {
+            int row = seat[0];
+            int col = seat[1];
+
+            map.put(row, map.getOrDefault(row, 0) | (1 << col));
+        }
+
+        // Seats 2,3,4,5
+        int left = (1 << 2) | (1 << 3) | (1 << 4) | (1 << 5);
+
+        // Seats 4,5,6,7
+        int middle = (1 << 4) | (1 << 5) | (1 << 6) | (1 << 7);
+
+        // Seats 6,7,8,9
+        int right = (1 << 6) | (1 << 7) | (1 << 8) | (1 << 9);
+
+        // Rows with no reservations can always fit 2 groups
+        int ans = (n - map.size()) * 2;
+
+        // Check rows having reservations
+        for (int mask : map.values()) {
+
+            boolean leftFree = (mask & left) == 0;
+            boolean middleFree = (mask & middle) == 0;
+            boolean rightFree = (mask & right) == 0;
+
+            if (leftFree && rightFree) {
+                ans += 2;
+            } else if (leftFree || middleFree || rightFree) {
+                ans += 1;
+            }
+        }
+
+        return ans;
+    }
+}
